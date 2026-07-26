92fbfc3648a3aea3a0c024167693a4632d48293c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-27 00:37:10 +0200

    fix

--- FenixWebGame.hpp
+++ FenixWebGame.hpp
@@ -9,10 +9,6 @@
 #include <backends/imgui_impl_sdl3.h>
 #include <backends/imgui_impl_opengl3.h>
 
-extern "C" {
-	#include <emscripten/emscripten.h>
-}
-
 namespace fe {
 
 	class FenixWebGame : public Game {
