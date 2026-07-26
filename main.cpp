e603a619d028a8b514c6872ab546b39d9075c66e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-26 18:06:01 +0200

    nope

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "window/SDLWindow.hpp"
 #include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
-#include <emscripten/log.h>
 #include <glad/glad.h>
 #include <iostream>
 #include <cstdio>
