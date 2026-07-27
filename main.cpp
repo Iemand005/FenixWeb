e1ab646e84f8f65ee37c8bb965cabcfdb5624556
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-27 05:08:06 +0200

    maybe just error for now and no thorw let it go see if it maybe jwroks anwyasy oh my god I'm stupid that's why ti don't wrk after switching to the options thing

--- main.cpp
+++ main.cpp
@@ -134,6 +134,7 @@ int main() {
     std::cout << "[init] CSS size: " << initW << "x" << initH << std::endl;
 
     fe::XRGameOptions options;
+    options.useVulkan = false; // TODO set vulkan false if glad loadproc is provided anways! or by defualt!;
     options.loadProc = reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress);
     g_game = new FenixWebGame(options);
     std::cout << "[init] FenixWebGame created" << std::endl;
